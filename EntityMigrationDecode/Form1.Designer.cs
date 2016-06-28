aa19c15ad5a128da006b642a4a1ff8866bb8dfeb
Evan Brandt <evanmbrandt@gmail.com>
2016-06-28 14:24:58 -0400

    Initial commit

---
+++ EntityMigrationDecode/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace EntityMigrationDecode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBase64String = new System.Windows.Forms.TextBox();
+            this.txtXmlOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDecode = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBase64String
+            // 
+            this.txtBase64String.Location = new System.Drawing.Point(11, 30);
+            this.txtBase64String.MaxLength = 131072;
+            this.txtBase64String.MinimumSize = new System.Drawing.Size(0, 100);
+            this.txtBase64String.Name = "txtBase64String";
+            this.txtBase64String.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtBase64String.Size = new System.Drawing.Size(602, 100);
+            this.txtBase64String.TabIndex = 0;
+            // 
+            // txtXmlOutput
+            // 
+            this.txtXmlOutput.Location = new System.Drawing.Point(11, 228);
+            this.txtXmlOutput.MaxLength = 131072;
+            this.txtXmlOutput.MinimumSize = new System.Drawing.Size(0, 200);
+            this.txtXmlOutput.Name = "txtXmlOutput";
+            this.txtXmlOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtXmlOutput.Size = new System.Drawing.Size(602, 200);
+            this.txtXmlOutput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Base64 Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "XML Output";
+            // 
+            // btnDecode
+            // 
+            this.btnDecode.Location = new System.Drawing.Point(11, 137);
+            this.btnDecode.Name = "btnDecode";
+            this.btnDecode.Size = new System.Drawing.Size(75, 23);
+            this.btnDecode.TabIndex = 4;
+            this.btnDecode.Text = "Decode";
+            this.btnDecode.UseVisualStyleBackColor = true;
+            this.btnDecode.Click += new System.EventHandler(this.btnDecode_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(538, 137);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 457);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDecode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtXmlOutput);
+            this.Controls.Add(this.txtBase64String);
+            this.Name = "Form1";
+            this.Text = "Decode Entity Migration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBase64String;
+        private System.Windows.Forms.TextBox txtXmlOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDecode;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ EntityMigrationDecode/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+
+namespace EntityMigrationDecode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtBase64String.Multiline = true;
+            txtXmlOutput.Multiline = true;
+        }
+
+        private void btnDecode_Click(object sender, EventArgs e)
+        {
+            string input = txtBase64String.Text;
+            string output = Decode(input);
+            txtXmlOutput.Text = output;
+
+            if (!Directory.Exists(@"c:\temp"))
+            {
+                Directory.CreateDirectory(@"c:\temp");
+            }
+            File.WriteAllText(@"c:\temp\edmx.xml", output);
+        }
+
+        private string Decode(string base64string)
+        {
+            try {
+                byte[] bytes = Convert.FromBase64String(base64string);
+                byte[] uncompressed = Decompress(bytes);
+
+                string edmx = Encoding.UTF8.GetString(uncompressed);
+                return edmx;
+            }
+            catch(Exception e)
+            {
+                return string.Format("Exception: {0}", e.Message);
+            }
+        }
+        private byte[] Decompress(byte[] gzip)
+        {
+            using (GZipStream stream = new GZipStream(new MemoryStream(gzip), CompressionMode.Decompress))
+            {
+                const int size = 4096;
+                byte[] buffer = new byte[size];
+
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    int count = 0;
+                    do
+                    {
+                        count = stream.Read(buffer, 0, size);
+                        if (count > 0)
+                        {
+                            memory.Write(buffer, 0, count);
+                        }
+                    }
+
+                    while (count > 0);
+                    return memory.ToArray();
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtBase64String.Text = string.Empty;
+            txtXmlOutput.Text = string.Empty;
+        }
+    }
+}
